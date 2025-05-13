9ce138465c5a7672a3a5d4309ca1135d0cc5d1a0
bblair321 <brady@BradysPC.>
2025-05-13 09:39:08 -0700

    added initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+const burgers = ['Hamburger','Cheeseburger'];
+const featuredDrink = 'Strawberry Milkshake';
+
+function addBurger(){
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+  if (true){
+    const anothernewBurger = 'Maple Bacon Burger';
+    burgers.push(anothernewBurger)
+  }
+}
+
+function changeFeaturedDrink(){
+  const featuredDrink = 'JavaShake';
+  console.log('Featured drink is now: ' + featuredDrink);
+}
